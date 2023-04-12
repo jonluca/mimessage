9e7e73dc4ae772780b76b61ba7dfd20e9d44d43a
JonLuca DeCaro <jonluca.decaro@gmail.com>
2023-04-12 13:19:22 -0700

    augment contacts

--- electron-src/types/contacts.d.ts
+++ electron-src/types/contacts.d.ts
@@ -1,45 +1,6 @@
 declare module "electron-mac-contacts" {
   export interface Contact {
-    identifier: string;
-    // optional
-    firstName?: string;
-    middleName?: string;
-    lastName?: string;
-    nickname?: string;
-    jobTitle?: string;
-    departmentName?: string;
-    organizationName?: string;
-    phoneNumbers?: string[];
-    emailAddresses?: string[];
-    socialProfiles?: string[];
-    instantMessageAddresses?: string[];
-    birthday?: string;
-    note?: string;
-    name?: string;
-    contactThumbnailImage?: Buffer;
-    contactImage?: Buffer;
     parsedName?: string;
     pngBase64?: string;
   }
-
-  export type OptionalProperties =
-    | "jobTitle"
-    | "departmentName"
-    | "organizationName"
-    | "middleName"
-    | "note"
-    | "contactImage"
-    | "contactThumbnailImage"
-    | "instantMessageAddresses"
-    | "socialProfiles";
-
-  export function requestAccess(): Promise<boolean>;
-  export function getAuthStatus(): string;
-  export function getAllContacts(extraProperties?: OptionalProperties[]): Promise<Contact[]>;
-  export function getContactsByName(name: string, extraProperties?: OptionalProperties[]): Promise<Contact[]>;
-  export function addNewContact(contact: Contact): Promise<Contact>;
-  export function updateContact(contact: Contact): Promise<Contact>;
-  export function deleteContact(contact: Contact): Promise<void>;
-
-  export const listener: Listener;
 }
