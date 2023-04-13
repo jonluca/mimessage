b6385627c3aad93380290e37b6d18e9b1fa3adf7
JonLuca DeCaro <jonluca.decaro@gmail.com>
2023-04-13 15:07:16 -0700

    add other dependency

--- src/components/wrapped/Charts.tsx
+++ src/components/wrapped/Charts.tsx
@@ -6,39 +6,28 @@ import { useWrappedDates } from "../../hooks/dataHooks";
 import { groupBy } from "lodash-es";
 import type { UserSerie } from "react-charts/types/types";
 import type { MessageDate } from "../../interfaces";
-import { SectionHeader, SectionWrapper } from "./Containers";
+import { SECTION_HEIGHT, SECTION_WIDTH, SectionHeader, SectionWrapper } from "./Containers";
 import Box from "@mui/material/Box";
+import dayjs from "dayjs";
 import { ErrorBoundary } from "../ErrorBoundary";
+import type { MessageDates } from "../../interfaces";
 const Chart = dynamic(() => import("react-charts").then((mod) => mod.Chart), {
   ssr: false,
 }) as typeof ChartType;
 
-export const MessagesByYear = () => {
+const MessagesByBase = ({
+  getDataCallback,
+}: {
+  getDataCallback: (dates: MessageDates | undefined) => UserSerie<{ primary: Date; secondary: number }>[];
+}) => {
   const { data: dates } = useWrappedDates();
 
   const data = React.useMemo(() => {
-    const grouped = groupBy(dates || [], (d: MessageDate) => {
-      if (d.date_obj) {
-        return d.date_obj.getFullYear();
-      }
-    });
-
-    const datums = Object.entries(grouped)
-      .filter((l) => l[0] !== "undefined")
-      .map(([year, dates]) => ({
-        primary: dates[0].date_obj!,
-        secondary: dates.length,
-      }));
-    return [
-      {
-        label: "Messages by Year",
-        data: datums,
-      },
-    ];
-  }, [dates]);
+    return getDataCallback(dates);
+  }, [dates, getDataCallback]);
 
   return (
-    <SectionWrapper sx={{ width: 550, height: 250 }}>
+    <SectionWrapper sx={{ width: SECTION_WIDTH, height: SECTION_HEIGHT }}>
       <SectionHeader>Messages by Year</SectionHeader>
       <Box sx={{ width: "100%", height: "100%" }}>
         <BaseChart data={data} />
@@ -46,12 +35,39 @@ export const MessagesByYear = () => {
     </SectionWrapper>
   );
 };
+const getMessagesByYear = (dates: MessageDates | undefined) => {
+  const grouped = groupBy(dates || [], (d: MessageDate) => {
+    if (d.date_obj) {
+      return d.date_obj.getFullYear();
+    }
+  });
+
+  const datums = Object.entries(grouped)
+    .filter((l) => l[0] !== "undefined")
+    .map(([_, dates]) => ({
+      primary: dayjs(dates[0].date_obj!).startOf("year").add(1, "d").toDate(),
+      secondary: dates.length,
+    }));
+  return [
+    {
+      label: "Messages by Year",
+      data: datums,
+    },
+  ];
+};
+export const MessagesByYear = () => {
+  return <MessagesByBase getDataCallback={getMessagesByYear} />;
+};
+
+export const MessagesByMonth = () => {
+  return <MessagesByBase getDataCallback={getMessagesByYear} />;
+};
 export const BaseChart = <T extends { primary: Date; secondary: number }>({ data }: { data: UserSerie<T>[] }) => {
   const primaryAxis = React.useMemo<AxisOptions<typeof data[number]["data"][number]>>(
     () => ({
       getValue: (datum) => datum.primary as Date,
       formatters: {
-        scale: (v: Date) => {
+        cursor: (v: Date) => {
           return v?.getFullYear().toString();
         },
       },
